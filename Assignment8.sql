6d82b247b8034958ebf6469b90fbb10de14fe3ea
aryark <aryark3112@gmail.com>
2023-08-22 16:15:20 +0530

    first commit

---
+++ Assignment8.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE AdvancedDb;
+USE AdvancedDb;
+
+
+CREATE TABLE Employees (
+    EmployeeId INT PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    BirthDate DATE,
+    Salary FLOAT
+);
+
+
+CREATE TABLE Products (
+    ProductId INT PRIMARY KEY,
+    ProductName NVARCHAR(50),
+    PDescription NVARCHAR(50),
+    ReleaseDate DATE,
+    Price FLOAT
+);
+
+
+CREATE TABLE Orders (
+    OrderId INT PRIMARY KEY,
+    OrderDate DATETIME,
+    Quantity SMALLINT,
+    Discount FLOAT,
+    IsShipped BIT
+);
+select * from Employees
+select * from Products
+select * from Orders
